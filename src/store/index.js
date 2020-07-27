64c2588a6f6a571b6440cf0f80fe3ca0dd2ad68e
Roberto Miguez <miguezroberto@gmail.com>
2020-07-27 10:58:34 +0100

    fix updateTickers Method

--- src/store/index.js
+++ src/store/index.js
@@ -8,10 +8,11 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     tickers: [],
-    tickersCalculated: [],
-    amount: 0
+    tickersCalculated: []
   },
   actions: {
+    // TODO should be this, unfortunately it didn't connect :(
+
     // loadTickers ({ commit }, currency) {
     //   const sdk = new SDK({
     //     baseUrl: 'https://api-sandbox.uphold.com',
@@ -22,27 +23,24 @@ export default new Vuex.Store({
     //   sdk.authorize('code')
     //     .then(() => sdk.getTicker())
     //     .then(tickers => {
-    //       const tickers = await tickers.filter(ticker => ticker.currency === currency)
+    //       const tickers = await tickers.filter(ticker => ticker.pair.slice(0, 3) === currency)
     //       commit('SET_TICKERS', tickers)
     //     })
     // }
 
     async loadTickers ({ commit }, currency) {
       try {
-        // console.warn(currency)
         const tickers = await db.filter(record => record.pair.slice(0, 3) === currency)
-        // console.warn(tickers)
         commit('SET_TICKERS', tickers)
       } catch (error) {
         // TODO Change to friendly message on page
         console.error('Loading error.')
       }
     },
-    updateTickers ({ commit, state }, amount) {
+    async updateTickers ({ commit, state }, amount) {
       try {
-        if (amount === 0 || amount === state.amount) return
-        state.amount = amount
-        const tickersCalculated = [...state.tickers.map(ticker => {
+        if (amount === 0) return
+        const tickersCalculated = await [...state.tickers.map(ticker => {
           return { ...ticker, ask: (ticker.ask * amount).toFixed(5) }
         })]
         commit('SET_TICKERS_CALCULATED', tickersCalculated)
